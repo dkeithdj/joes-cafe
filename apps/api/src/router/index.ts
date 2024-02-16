676cd44f83ae124565fdc18516b9a80f41397a76
dkeithdj <42316655+dkeithdj@users.noreply.github.com>
2024-02-17 02:17:43 +0800

    api: add additional endpoints

--- apps/api/src/router/index.ts
+++ apps/api/src/router/index.ts
@@ -9,6 +9,10 @@ const t = initTRPC.context<Context>().create();
 export const router = t.router;
 export const publicProcedure = t.procedure;
 
+/* TODO: separate mutations and queries
+ * <table>.<get/post/put/patch/delete>.<filter, e.g. by id, name, etc.>
+ */
+
 export const appRouter = router({
   hello: publicProcedure.query(({ ctx, input }) => {
     return { greeting: "hello world" };
@@ -34,7 +38,7 @@ export const appRouter = router({
     .input(
       z.object({
         name: z.string(),
-      })
+      }),
     )
     .mutation(async ({ ctx, input }) => {
       const createTransaction = await prisma.transaction.create({
@@ -51,14 +55,17 @@ export const appRouter = router({
         },
       });
       ctx.res
-        .cookie("customer.transaction", createTransaction.id, {
+        .setCookie("customer.transaction", createTransaction.id, {
           maxAge: 60 * 60 * 24,
+          path: "/",
         })
-        .cookie("customer.customer", createTransaction.customer.id, {
+        .setCookie("customer.customer", createTransaction.customer.id, {
           maxAge: 60 * 60 * 24,
+          path: "/",
         })
-        .cookie("customer.name", createTransaction.customer.name, {
+        .setCookie("customer.name", createTransaction.customer.name, {
           maxAge: 60 * 60 * 24,
+          path: "/",
         });
       return createTransaction;
     }),
@@ -70,10 +77,10 @@ export const appRouter = router({
         category: z.string(),
         image: z.string(),
         isAvailable: z.boolean(),
-      })
+      }),
     )
-    .mutation((opts) => {
-      return prisma.product.create({
+    .mutation(async (opts) => {
+      return await prisma.product.create({
         data: {
           name: opts.input.name,
           price: opts.input.price,
@@ -92,10 +99,10 @@ export const appRouter = router({
     .input(
       z.object({
         status: z.enum([Status.Declined, Status.Completed, Status.Processing]),
-      })
+      }),
     )
-    .query((opts) => {
-      return prisma.order.findMany({
+    .query(async (opts) => {
+      return await prisma.order.findMany({
         where: {
           status: {
             status: opts.input.status,
@@ -143,13 +150,135 @@ export const appRouter = router({
         },
       });
     }),
+  createOrder: publicProcedure
+    .input(
+      z.object({
+        tableId: z.string(),
+        transactionId: z.string(),
+        totalAmount: z.number(),
+      }),
+    )
+    .mutation(async ({ input, ctx }) => {
+      const { tableId, transactionId, totalAmount } = input;
+
+      const order = await prisma.order.create({
+        data: {
+          transaction: {
+            connect: {
+              id: transactionId,
+            },
+          },
+          table: {
+            connect: {
+              id: tableId,
+            },
+          },
+          status: {
+            connect: {
+              id: 1,
+            },
+          },
+          totalAmount: totalAmount,
+        },
+      });
+      return order;
+    }),
   getStatus: publicProcedure.query(() => {}),
-  getStaff: publicProcedure.query(() => {
-    return prisma.staff.findMany();
+  getStaff: publicProcedure.query(async () => {
+    return await prisma.staff.findMany();
   }),
-  getCategories: publicProcedure.query(({ input, ctx }) => {
-    return prisma.category.findMany();
+  getCategories: publicProcedure.query(async ({ input, ctx }) => {
+    return await prisma.category.findMany();
   }),
+  addItem: publicProcedure
+    .input(
+      z.object({
+        // itemId: z.string(),
+        productId: z.string(),
+        transactionId: z.string(),
+        customerId: z.string(),
+      }),
+    )
+    .mutation(async ({ input, ctx }) => {
+      const { productId, transactionId, customerId } = input;
+      // if (!itemId) {
+      const items = await prisma.items.create({
+        data: {
+          product: {
+            connect: {
+              id: productId,
+            },
+          },
+          transaction: {
+            connectOrCreate: {
+              where: {
+                id: transactionId,
+              },
+              create: {
+                customer: {
+                  connect: {
+                    id: customerId,
+                  },
+                },
+              },
+            },
+          },
+        },
+      });
+    }),
+  getItem: publicProcedure
+    .input(z.object({ transactionId: z.string() }))
+    .query(async ({ input, ctx }) => {
+      const itemsView = await prisma.itemsView.findMany({
+        where: { transactionId: input.transactionId },
+      });
+
+      return itemsView;
+    }),
+  deleteItem: publicProcedure
+    .input(z.object({ itemId: z.string() }))
+    .mutation(async ({ input, ctx }) => {
+      const deleteItem = await prisma.items.delete({
+        where: {
+          id: input.itemId,
+        },
+      });
+      return deleteItem;
+    }),
+  updateTransaction: publicProcedure
+    .input(
+      z.object({
+        id: z.string(),
+        name: z.string(),
+      }),
+    )
+    .mutation(async ({ input, ctx }) => {
+      const updateTransaction = await prisma.transaction.create({
+        data: {
+          customer: {
+            connectOrCreate: {
+              where: {
+                id: input.id,
+              },
+              create: {
+                name: input.name,
+              },
+            },
+          },
+        },
+      });
+
+      // ctx.res
+      //   .setCookie("customer.transaction", createTransaction.id, {
+      ctx.res.setCookie("customer.transaction", updateTransaction.id, {
+        maxAge: 60 * 60 * 24,
+      });
+      ctx.res.setCookie("customer.customer", updateTransaction.customerId, {
+        maxAge: 60 * 60 * 24,
+      });
+
+      return updateTransaction;
+    }),
   randomNumber: publicProcedure.subscription(() => {
     return observable<{ randomNumber: number }>((emit) => {
       const timer = setInterval(() => {
