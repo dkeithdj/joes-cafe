15fd9c35608cbd6aca067c6bf6cc8f8fbdb6153f
dkeithdj <42316655+dkeithdj@users.noreply.github.com>
2024-02-27 04:36:26 +0800

    kitchen: make kitchen update realtime

--- apps/kitchen/app/page.tsx
+++ apps/kitchen/app/page.tsx
@@ -1,135 +1,91 @@
+"use client";
 import KitchenOrder from "@kitchen/components/KitchenOrder";
-import _Orders from "@kitchen/components/_Orders";
+import { trpc } from "@kitchen/hooks/trpc";
 import { Card, CardDescription, CardHeader } from "@ui/components/ui/card";
-import Image from "next/image";
-
-const orderTemp = [
-  {
-    id: "asdfasdf",
-    name: "Test Order",
-    table: "Table 1",
-    status: "incoming",
-    items: [
-      {
-        id: "wftr",
-        name: "Test Item",
-        quantity: 2,
-      },
-      {
-        id: "aa",
-        name: "Test Item 1",
-        quantity: 1,
-      },
-      {
-        id: "he",
-        name: "Test Item 2",
-        quantity: 3,
-      },
-    ],
-  },
-  {
-    id: "hiae",
-    name: "Test Order 1",
-    table: "Table 23",
-    status: "ongoing",
-    items: [
-      {
-        id: "wftr",
-        name: "Test Item",
-        quantity: 2,
-      },
-      {
-        id: "aa",
-        name: "Test Item 1",
-        quantity: 1,
-      },
-      {
-        id: "he",
-        name: "Test Item 2",
-        quantity: 3,
-      },
-    ],
-  },
-  {
-    id: "aai",
-    name: "Test Order 2",
-    table: "Table 11",
-    status: "completed",
-    items: [
-      {
-        id: "wftr",
-        name: "Test Item",
-        quantity: 2,
-      },
-      {
-        id: "aa",
-        name: "Test Item 1",
-        quantity: 1,
-      },
-      {
-        id: "he",
-        name: "Test Item 2",
-        quantity: 3,
-      },
-    ],
-  },
-  {
-    id: "hoe",
-    name: "Test Order 3",
-    table: "Table 12",
-    status: "completed",
-    items: [
-      {
-        id: "wftr",
-        name: "Test Item",
-        quantity: 2,
-      },
-      {
-        id: "aa",
-        name: "Test Item 1",
-        quantity: 1,
-      },
-      {
-        id: "he",
-        name: "Test Item 2",
-        quantity: 3,
-      },
-    ],
-  },
-];
 
 const KitchenPage = () => {
+  const utils = trpc.useUtils();
+
+  const { data: orderData, isSuccess } = trpc.getKitchenOrders.useQuery();
+
+  trpc.onUpdateOrder.useSubscription(undefined, {
+    onData: (data) => {
+      utils.getKitchenOrders.invalidate();
+    },
+  });
+
+  const { mutate: updateOrder } = trpc.updateKitchenOrder.useMutation({
+    onSuccess: (data) => {
+      utils.getKitchenOrders.invalidate();
+    },
+  });
+
+  // moves from Preparing -> Brewing
+  const handleIncoming = (orderId: string) => {
+    // 6: Brewing
+    updateOrder({ orderId: orderId, statusId: "6" });
+  };
+  // moves from Brewing -> Completed
+  const handleOnGoing = (orderId: string) => {
+    // 2: Completed
+    updateOrder({ orderId: orderId, statusId: "2" });
+  };
+  // can view the status/info of item
+  const handleCompleted = (orderId: string) => {
+    console.log(`Order ${orderId} Served`);
+  };
   return (
     <div className="grid sm:grid-cols-1 lg:grid-cols-3 gap-4 sm:gap-2 justify-items-center">
       <Card>
         <CardHeader className="bg-[#D2B48C] rounded-md rounded-b-none">
           Incoming
         </CardHeader>
-        {orderTemp
-          .filter((filter) => filter.status === "incoming")
-          .map((order) => (
-            <KitchenOrder order={order} />
-          ))}
+        {isSuccess &&
+          orderData
+            .filter((filter) => filter.status.status.toString() === "Preparing")
+            .map((order, i) => (
+              <KitchenOrder
+                key={i}
+                order={order}
+                handleOrder={handleIncoming}
+                text={"Accept"}
+                color={"#D2B48C"}
+              />
+            ))}
       </Card>
       <Card>
-        <CardHeader className="bg-[#D2B48C] rounded-md rounded-b-none">
+        <CardHeader className="bg-[#B09478] rounded-md rounded-b-none">
           On-going
         </CardHeader>
-        {orderTemp
-          .filter((filter) => filter.status === "ongoing")
-          .map((order) => (
-            <KitchenOrder order={order} />
-          ))}
+        {isSuccess &&
+          orderData
+            .filter((filter) => filter.status.status.toString() === "Brewing")
+            .map((order, i) => (
+              <KitchenOrder
+                key={i}
+                order={order}
+                handleOrder={handleOnGoing}
+                text={"Complete"}
+                color={"#B09478"}
+              />
+            ))}
       </Card>
       <Card>
-        <CardHeader className="bg-[#D2B48C] rounded-md rounded-b-none">
+        <CardHeader className="bg-[#603D04] rounded-md rounded-b-none">
           Completed
         </CardHeader>
-        {orderTemp
-          .filter((filter) => filter.status === "completed")
-          .map((order) => (
-            <KitchenOrder order={order} />
-          ))}
+        {isSuccess &&
+          orderData
+            .filter((filter) => filter.status.status.toString() === "Completed")
+            .map((order, i) => (
+              <KitchenOrder
+                key={i}
+                order={order}
+                handleOrder={handleCompleted}
+                text={"Order Served"}
+                color={"#603D04"}
+              />
+            ))}
       </Card>
     </div>
   );
--- apps/kitchen/components/KitchenOrder.tsx
+++ apps/kitchen/components/KitchenOrder.tsx
@@ -3,53 +3,65 @@ import { CardContent } from "@ui/components/ui/card";
 import { Button } from "@ui/components/ui/button";
 import { Separator } from "@ui/components/ui/separator";
 import React from "react";
+import { RouterOutputs } from "@repo/api";
+import { trpc } from "@kitchen/hooks/trpc";
+
+type OrderOptions = RouterOutputs["getKitchenOrders"][0];
 
 const KitchenOrder = ({
   order,
+  handleOrder,
+  text,
+  color,
 }: {
-  order: {
-    id: string;
-    name: string;
-    table: string;
-    status: string;
-    items: {
-      id: string;
-      name: string;
-      quantity: number;
-    }[];
-  };
+  order: OrderOptions;
+  handleOrder: (e: string) => void;
+  text: string;
+  color: string;
 }) => {
+  const { data: itemsData, isSuccess } = trpc.getKitchenItems.useQuery({
+    transactionId: order.transaction.id,
+  });
   return (
-    <div className="bg-[#D2B48C] rounded-lg m-2">
+    <div className={`bg-[${color}] rounded-lg m-2`}>
       <CardContent className="py-2">
         <table>
-          <tr>
-            <td className="w-[15ch]">Order Number:</td>
-            <td className="w-[10ch] text-end">{order.id}</td>
-          </tr>
-          <tr>
-            <td className="w-[15ch]">Name:</td>
-            <td className="w-[10ch] text-end">{order.name}</td>
-          </tr>
-          <tr>
-            <td className="w-[15ch]">Table:</td>
-            <td className="w-[10ch] text-end">{order.table}</td>
-          </tr>
+          <tbody>
+            <tr>
+              <td className="w-[8ch] text-ellipsis">Order #:</td>
+              <td className="max-w-[17ch] text-end truncate">{order.id}</td>
+            </tr>
+            <tr>
+              <td className="w-[8ch]">Name:</td>
+              <td className="w-[17ch] text-end">
+                {order.transaction.customer.name}
+              </td>
+            </tr>
+            <tr>
+              <td className="w-[8ch]">Table:</td>
+              <td className="w-[17ch] text-end">{order.table.number}</td>
+            </tr>
+          </tbody>
         </table>
       </CardContent>
       <Separator className="w-[90%] mx-auto" />
       <CardContent className="py-2">
         <table>
-          {order.items.map((item) => (
-            <tr className="">
-              <td className="w-[15ch]">{item.name}</td>
-              <td className="w-[10ch] text-end">x{item.quantity}</td>
-            </tr>
-          ))}
+          <tbody>
+            {isSuccess &&
+              itemsData.map((item, i) => (
+                <tr className="" key={i}>
+                  <td className="w-[20ch]">{item.productname}</td>
+                  <td className="w-[5ch] text-end">x{item.totalquantity}</td>
+                </tr>
+              ))}
+          </tbody>
         </table>
       </CardContent>
       <CardContent className="py-2">
-        <Button>[dynamic]</Button>
+        <Button className="w-full" onClick={() => handleOrder(order.id)}>
+          {text}
+        </Button>
       </CardContent>
     </div>
   );
