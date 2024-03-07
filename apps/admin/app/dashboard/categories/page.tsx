d06f21b4e48b3a628a4b4b5571e35ce52b72d449
denreikeith <42316655+dkeithdj@users.noreply.github.com>
2024-03-08 02:06:12 +0800

    admin: add category tab

---
+++ apps/admin/app/dashboard/categories/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const CategoriesPage = () => {
+  return (
+    <div className="w-auto mx-14">
+      <div className="flex flex-row justify-between">
+        <div className="text-6xl text-[#603D04] py-4 ">Categories</div>
+      </div>
+      <div>content</div>
+    </div>
+  );
+};
+
+export default CategoriesPage;
