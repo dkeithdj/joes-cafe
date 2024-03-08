0619071040a3262c98a3f26728783556cb7ecb42
denreikeith <42316655+dkeithdj@users.noreply.github.com>
2024-03-09 07:30:57 +0800

    admin: created categories page

--- apps/admin/app/dashboard/categories/page.tsx
+++ apps/admin/app/dashboard/categories/page.tsx
@@ -2,18 +2,54 @@
 import { trpc } from "@admin/hooks/trpc";
 import { Card } from "@ui/components/ui/card";
 import { Button } from "@ui/components/ui/button";
-import React from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@ui/components/ui/dialog";
+import React, { useState } from "react";
+import Loading from "@admin/components/Loading";
+import { toast } from "sonner";
+import AddCategoryCard from "@admin/components/AddCategoryCard";
+import EditCategoryCard from "@admin/components/EditCategoryCard";
 
 const CategoriesPage = () => {
-  const { data: categories } = trpc.getCategories.useQuery();
+  const [availability, setAvailability] = useState(true);
+  const [open, setOpen] = useState(false);
+  const utils = trpc.useUtils();
 
-  const { data: categoryByProduct, isSuccess } =
-    trpc.getCategoriesAndProductCount.useQuery();
+  const {
+    data: categoryByProduct,
+    isSuccess,
+    isLoading,
+  } = trpc.getCategoriesAndProductCount.useQuery(availability);
 
-  const { mutate } = trpc.updateCategory.useMutation();
-  console.log(categoryByProduct);
+  const { mutate: createCategory } = trpc.createCategory.useMutation({
+    onSuccess() {
+      utils.getCategoriesAndProductCount.invalidate();
+      utils.getCategories.invalidate();
+      toast.success("Added Category");
+    },
+    onError(err) {
+      toast.error(err.message);
+    },
+  });
 
-  const addCategory = () => {};
+  const helperCreateCategory = ({
+    name,
+    isAvailable,
+  }: {
+    name: string;
+    isAvailable: boolean;
+  }) => {
+    createCategory({
+      name: name,
+      isAvailable: isAvailable,
+    });
+  };
 
   return (
     <div className="w-auto mx-14">
@@ -21,20 +57,55 @@ const CategoriesPage = () => {
         <div className="text-6xl text-[#603D04] py-4 ">Categories</div>
 
         <div className="flex flex-row items-center gap-x-4">
-          <Button onClick={addCategory}>Add Category</Button>
+          <Dialog open={open} onOpenChange={setOpen}>
+            <DialogTrigger asChild>
+              <div className="flex flex-row items-center gap-x-4">
+                <Button>Add Category</Button>
+              </div>
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-[425px]">
+              <DialogHeader>
+                <DialogTitle>Add Category</DialogTitle>
+              </DialogHeader>
+              <AddCategoryCard
+                mutate={helperCreateCategory}
+                setOpen={setOpen}
+              />
+            </DialogContent>
+          </Dialog>
         </div>
       </div>
-      <div>
+
+      <div className="flex pb-4">
+        <div className="w-fit h-10 items-center justify-center rounded-md bg-[#E1CDAD] p-1 text-muted-foreground">
+          <button
+            disabled={availability}
+            onClick={() => setAvailability(true)}
+            className="inline-flex disabled:bg-[#512711] disabled:text-[#E1CDAD] items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none  "
+          >
+            Available
+          </button>
+          <button
+            disabled={!availability}
+            onClick={() => setAvailability(false)}
+            className="inline-flex disabled:bg-[#512711] disabled:text-[#E1CDAD] items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none "
+          >
+            Unavailable
+          </button>
+        </div>
+      </div>
+      {isLoading && (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          <Loading length={6} height="150" />
+        </div>
+      )}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {isSuccess &&
           categoryByProduct
+            // .filter((category) => availability === category.isAvailable)
             .sort((a, b) => a.name.localeCompare(b.name))
             .map((category, id) => (
-              <Card key={id}>
-                <p>{category.id}</p>
-                <p>{category.name}</p>
-                <p>{category.isAvailable && "yus"}</p>
-                <p>{category._count.products}</p>
-              </Card>
+              <EditCategoryCard key={id} category={category} />
             ))}
       </div>
     </div>
